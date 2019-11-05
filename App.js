f1b3156516174e5c77f8f56dbcfd8acfe391215e
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-11-05 11:25:34 +0000

    converting App.js to hooks based architecture

--- App.js
+++ App.js
@@ -1,60 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View, Animated } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { API_KEY } from './utils/WeatherAPIKey';
-import Weather from './components/Weather';
+// import Weather from './components/Weather';
 
-export default class App extends React.Component {
-  state = {
-    isLoading: false,
-    temperature: 0,
-    weatherCondition: null,
-    error: null
-  };
+const App = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [temperature, setTemperature] = useState(0);
+    const [weatherCondition, setWeatherCondition] = useState(null);
+    const [error, setError] = useState(null);
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        this.fetchWeather(position.coords.latitude, position.coords.longitude);
-      },
-      error => {
-        this.setState({
-          error: 'Error Gettig Weather Condtions'
-        });
-      }
-    );
-  }
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                fetchWeather(51.515579, -0.128360);
+            },
+            error => {
+                setError('Error getting Weather Conditions');
+            }
+        );
+    });
 
-  fetchWeather(lat = 25, lon = 25) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
-    )
-      .then(res => res.json())
-        .then(json => {
-          // console.log(json);
-          this.setState({
-            temperature: json.main.temp,
-            weatherCondition: json.weather[0].main,
-            isLoading: false
-          });
+    const fetchWeather = (lat, lon) => {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+        )
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+            });
+    };
 
-      });
-  }
 
-  render() {
-    const { isLoading, weatherCondition, temperature } = this.state;
     return (
-      <View style={styles.container}>
-        {isLoading ? <Text>Fetching The Weather</Text> : <Weather weather={weatherCondition} temperature={temperature} />}
-      </View>
+        <View style={styles.container}>
+            {isLoading ? (
+                <View style={styles.loadingContainer}>
+                    <Text style={styles.loadingText}>Fetching the Weather...</Text>
+                </View>
+            ) : (
+                    <View style={styles.loadingContainer}>
+                        <Text style={styles.loadingText}>All good...</Text>
+                    </View>
+                )}
+        </View>
     );
-  }
-};
-
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    loadingContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFFDE4'
+    },
+    loadingText: {
+        fontSize: 30,
+    }
 });
+
+export default App;
